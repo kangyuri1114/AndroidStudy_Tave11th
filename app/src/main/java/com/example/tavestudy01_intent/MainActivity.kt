82b0d796389e57576c695213337b6a2fc40e46e2
kangyuri1114 <yuri3334@gmail.com>
2023-03-23 22:27:10 +0900

    commit3 - 명시적 인텐트 실습 1. 화면 전환 2. key, value 넘기기

--- app/src/main/java/com/example/tavestudy01_intent/MainActivity.kt
+++ app/src/main/java/com/example/tavestudy01_intent/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.tavestudy01_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoSubActivity: Button = findViewById(R.id.btnGoSubActivity)
+        btnGoSubActivity.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnThrowValue: Button = findViewById(R.id.btnThrowValue)
+        val intent2 = Intent(this, SubActivity::class.java)
+        intent2.putExtra("Data1", "Hello World")
+        intent2.putExtra("Data2", 2023)
+        btnThrowValue.setOnClickListener{startActivity(intent2)}
     }
 }
