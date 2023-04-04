f0de0fce58a4c64fa4d374fc669ef9601025b145
kangyuri1114 <yuri3334@gmail.com>
2023-04-05 01:33:05 +0900

    commit3 Activity LifeCycle Toast 출력

--- app/src/main/java/com/example/tavestudy01_intent/MainActivity.kt
+++ app/src/main/java/com/example/tavestudy01_intent/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //생명주기 예제
+        Toast.makeText(this, "onCreate 호출 됨", Toast.LENGTH_LONG).show()
 
         val btnGoSubActivity: Button = findViewById(R.id.btnGoSubActivity)
         btnGoSubActivity.setOnClickListener {
@@ -21,5 +24,29 @@ class MainActivity : AppCompatActivity() {
         intent2.putExtra("Data1", "Hello World")
         intent2.putExtra("Data2", 2023)
         btnThrowValue.setOnClickListener{startActivity(intent2)}
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart 호출 됨", Toast.LENGTH_LONG).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume 호출 됨", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause 호출 됨", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop 호출 됨", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart 호출 됨", Toast.LENGTH_LONG).show()
     }
 }
