75d7e4943b0756c8e5aa95630110f44703aa0bec
kangyuri1114 <yuri3334@gmail.com>
2023-04-14 16:51:52 +0900

    commit 3 pause, clear 구현

--- app/src/main/java/com/example/tavestudy03_coroutine/MainActivity.kt
+++ app/src/main/java/com/example/tavestudy03_coroutine/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
 
     var total = 0
-    val started = false
+    var started = false
 
     //핸들러 : 화면의 시간 값 출력
     val handler = object: Handler(Looper.getMainLooper()){
@@ -21,8 +21,9 @@ class MainActivity : AppCompatActivity() {
             super.handleMessage(msg) //thread를 통해 값을 쓴다.
             val minute = String.format("%02d",total/60)  //앞의 두 자리를 나타내는
             val second = String.format("%02d",total%60)
+            val millisecond = String.format("%02d",total/1000)
 
-            binding.timer.text = "$minute : $second"
+            binding.timer.text = "$minute : $second : $millisecond"
         }
     }
 
@@ -31,15 +32,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
        binding.start.setOnClickListener(){
+           started = true
+
            thread(start = true){
-
-           while(true) { //무한루프로 1씩 증가된 값을 사용
+           while(started) { //무한루프로 1씩 증가된 값을 사용
                Thread.sleep(1000) //1초씩 쉬기
+                if(started) { //stop 시에도 반복문이 돌 수 있기 때문에 started 한번 더 작성
+                    total += 1
+                    handler?.sendEmptyMessage(0) //실행하기 위해서 아무것도 없는 값을 handler에 부여
+                    }
+                }
+           }
+       }
 
-               total += 1
-               handler?.sendEmptyMessage(0) //실행하기 위해서 아무것도 없는 값을 handler에 부여
+       binding.pause.setOnClickListener(){
+           if(started){
+               started = false
+               total = 0
            }
-           }
+       }
+
+       binding.clear.setOnClickListener(){
+
+           started = false
+           total = 0
+           binding.timer.text = "00:00:00"
+
        }
     }
 }
