438a0b18d8d95d388d630bf34a42229b5ad35b41
kangyuri1114 <yuri3334@gmail.com>
2023-04-15 16:08:40 +0900

    commit 5 - Toast Message 추가(실행 조건 추가) - 코드 정리

--- app/src/main/java/com/example/tavestudy03_coroutine/MainActivity.kt
+++ app/src/main/java/com/example/tavestudy03_coroutine/MainActivity.kt
@@ -2,88 +2,97 @@ package com.example.tavestudy03_coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.view.View
+import android.widget.Toast
 import com.example.tavestudy03_coroutine.databinding.ActivityMainBinding
-import java.util.* //Timer 클래스를 이용한 스케줄링 사용하기 위해 Import
-import kotlin.concurrent.thread
+import java.util.*
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    //멤버 변수로 ActivityMainBinding 객체의 변수를 선언
-    private lateinit var binding: ActivityMainBinding
-
-    //사용할 변수들 선언
-    var isRunning = false
-    var Timer: Timer? = null
-    var total = 0
-
+    // private 선언 이유 - 캡슐화(보안성, 메모리 누수 방지)
+    private var isRunning = false
+    private var timer: Timer? = null
+    private var total = 0
 
    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-       //ActivityMainBiding 객체를 구해서 ContentView로 등록
-       binding = ActivityMainBinding.inflate(layoutInflater)
        setContentView(binding.root)
-       //이후에 각각의 위젯을 위젯에 binding.아이디로 접근 가능
 
-
-       fun start(){
-           binding.start.text="일시정지"
-           isRunning = true
-
-           Timer = timer(period = 10){
-               //코틀린에서 제공하는 timer 함수로 일정한 주기로 반복하는 동작을 수행할 때 사용됨.
-               //10밀리초 단위로 시간을 증가시키도록 작성. 10밀리초 단위의 타이머 동작.
-               // 1millisec = 1/1000 sec
-               total++ //시간진행 시작
-
-               //분, 초, 밀리초
-               val minute = total/6000
-               val second = (total%6000)/100
-               val millisecond = total % 100
-
-               //UI 스레드
-               runOnUiThread{ //runOnUiThread를 사용 -> UI 작업이 백그라운드 스레드가 아닌 UI 스레드(메인 스레드)에서 일어남
-                   if(isRunning) { //isRunning이 true일 경우에만 UI가 업데이트
-                       binding.timer.text = "${minute} : ${second} : ${millisecond}"
-                   }
-               }
-           }
-       }
-
-       fun pause(){
-           binding.start.text = "시작"
-
-           isRunning = false
-           Timer?.cancel() //타이머 멈추기
-       }
-
-       fun clear(){
-           Timer?.cancel()
-
-           binding.start.text = "시작"
-           isRunning = false
-
-           total = 0
-           binding.timer.text = "00:00:00"
-       }
-
-
-       //버튼 구현
-       //START 버튼
-       binding.start.setOnClickListener() {
-           if (isRunning) { //false인 경우 pause()함수 실행
+       /**
+        * Start 버튼
+        *  Condition(isRunning) - false인 경우 실행
+        * */
+       binding.start.setOnClickListener {
+           if (isRunning) {
                pause()
            } else start()
        }
 
-       //Clear 버튼 구현
-       binding.clear.setOnClickListener(){
-           clear() //clear 함수 실행
-       }
+       /**
+        * Clear 버튼
+        *  Timer Stop & Reset
+        *  00:00:00 로 초기화
+        */
+       binding.clear.setOnClickListener { clear() }
+    }
 
+
+
+    /**
+     * Tip) 1millis = 1/1000 sec
+     *
+     * Timer =
+     *  코틀린에서 제공하는 timer 함수로 일정한 주기로 반복하는 동작을 수행할 때 사용됨.
+     *  '10밀리초 단위로 시간을 증가시키도록 작성. 10밀리초 단위의 타이머 동작.
+     * total++ : 시간 진행
+     *
+     * */
+    private fun start() {
+        binding.start.text="일시정지"
+        isRunning = true
+
+        timer = timer(period = 10){
+            total++
+
+            val minute = total/6000
+            val second = (total%6000)/100
+            val millisecond = total % 100
+
+            runOnUiThread{
+                if (isRunning) {
+                    binding.timer.text =
+                        String.format("%02d : %02d : %02d", minute, second, millisecond)
+
+                    if (minute == 99 && second == 59 && millisecond == 99) {
+                        pause()
+                        Toast.makeText(
+                            applicationContext,
+                            "타이머를 중지합니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * pause()
+     *  - Timer.cancel() : 시간초 정지(타이머 정지지)
+     * */
+   private fun pause(){
+        binding.start.text = "시작"
+        isRunning = false
+        timer?.cancel()
+    }
+
+    private fun clear(){
+        timer?.cancel()
+        binding.start.text = "시작"
+        isRunning = false
+        total = 0
+        binding.timer.text = getText(R.string.timer)
     }
 }
